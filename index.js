f404fd4c39399295595001d332dbd937d2d40855
Thainara88 <thainara.franca@escola.pr.gov.br>
2024-08-12 23:09:48 +0000

    12/08/2024

---
+++ index.js
@@ -0,0 +1,14 @@
+let idade =19;
+const anoNascimento = 2004 
+let anoAtual = 2024;
+const nomeCompleto = "Thainara Ferreira"
+
+console.log("Meu nome é"+ nomeCompleto + nomeCompleto + "nascido no ano de" + anoNascimento + " e no ano atual " + anoAtual + "completo" + idade + "anos" );
+anoAtual = anoAtual + 1; 
+
+console.log(anoAtual);
+console.log(anoAtual);
+
+idade=idade + 1; 
+console.log(idade); 
+console.log(idade);
